e39619f8ba6cfafc5bd664a8050c3d99d1815733
melissamorataya <User@DESKTOP-D4JTUSA>
2024-07-26 22:54:08 -0600

    tarea

---
+++ tarea/tarea/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mi_primer_proyecto
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            //prioridad de los operadores aritmeticos
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 };
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma += num;
+
+            }
+            double prom = suma + suma / serie.Length;
+            Console.WriteLine("la suma es : {0}, el promedio {1}", suma, prom);
+            // pausa
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
